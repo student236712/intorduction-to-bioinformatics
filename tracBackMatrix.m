d9b93feb35fa3f4326628c683646d524ef9659f5
student236712 <43950363+student236712@users.noreply.github.com>
2019-04-25 20:32:55 +0300

    Poprawiona funkcja szukania optymalnej sciezki

--- tracBackMatrix.m
+++ tracBackMatrix.m
@@ -1,43 +1,39 @@
-function [aligmentMatrix] = tracBackMatrix(scorringMatrix,mode)
+function [aligmentMatrix] = tracBackMatrix(R,match,mismatch,gap,sequence1,sequence2)
 %Funkcja generuj¹ca macierz ze œcie¿k¹ optymalnego dopasowania (poruszanie
 %siê po 1 odpowiada œcie¿ce)
 
 %Argumenty wejœciowe:
-%scorringMatrix - macierz punktacji dla dwóch sekwencji
-%mode - tryb porównania sekwencji: D - distance, S - similarity
+%R - macierz punktacji dla dwóch sekwencji
+%match - iloœæ punktów przyznawana za zgodnoœæ pary
+%mismatch - iloœæ punktów przyznawana za niezgodnoœæ pary
+%gap - iloœæ punktów przyznawana za przerwê
+%sequence1,sequence2 - sekwencje porównywane
 
-[n,m] =  size(scorringMatrix);
-aligmentMatrix =  zeros(n,m);
+
+[i,j] =  size(R);
+aligmentMatrix =  zeros(i,j);
 aligmentMatrix(end) =  1;
-  
+H = zeros(1,3);
 
-while n >=  2 && m >= 2
-        point =  scorringMatrix(n-1,m-1);
-        i =  n-1;
-        j =  m-1;
-        if mode == 'D'
-            if(point > scorringMatrix(n,m-1))
-                i =  n;
-                j =  m-1;
-            end
-            if(point > scorringMatrix(n-1,m))
-                i =  n-1;
-                j =  m;
-            end
-                       
-        elseif mode == 'S'
-            if(point < scorringMatrix(n,m-1))
-                i =  n;
-                j =  m-1;
-            end
-            if(point < scorringMatrix(n-1,m))
-                i =  n-1;
-                j =  m;
-            end            
-        end
-         
+while i >=  2 || j >= 2
+    H(1) = R(i-1,j-1);
+    H(2) = R(i-1,j);
+    H(3) = R(i,j-1);
+    
+    if ((R(i,j)-H(1)) == match ) && (sequence1(i) == sequence2(j))
+        i = i-1;
+        j = j-1;
+        
+    elseif ((R(i,j)-H(1)) == mismatch ) && (sequence1(i) ~= sequence2(j))
+        i = i-1;
+        j = j-1;
+        
+    elseif (R(i,j)- H(2) == gap)
+        i = i-1;
+    elseif (R(i,j) - H(3) == gap)
+        j = j-1;
+    end
+    
     aligmentMatrix(i,j) = 1;
-    n = i; 
-    m = j;
 end
 end
