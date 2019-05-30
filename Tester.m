75b861fafc3f64eb7267295ec09cc44b3cf09bf7
student236712 <43950363+student236712@users.noreply.github.com>
2019-05-30 13:00:45 +0200

    Skrypt testujący

--- Tester.m
+++ Tester.m
@@ -1,12 +1,12 @@
 % % %Przyk³adowe u¿ycie programu za pomoc¹ linii komend:
 %pliki wczytane rêcznie
-MultipleSequencesAlignment(0,1,2,4,'MSAalignment.txt')
+% MultipleSequencesAlignment(0,1,1,4,'MSAalignment.txt')
 %pliki wczytane z bazy NCBI
 MultipleSequencesAlignment(0,1,2,5,'MSAalignment.txt',...
-    'X75583.1','X75589.1','X75584.1','X75586.1','X75581.1');
+    'X75583.1','X75589.1','X75586.1','X75581.1','X75584.1');
 %pliki wczytane z plikow tekstowych (dla ulatwienia korzystania z
 %programu wszystkie pliki znajduja sie w jednym folderze)
-MultipleSequencesAlignment(0,1,2,4,'MSAalignment.txt',...
-    'C:\Nauka\6\Wstêp do bioinformatyki\Lab\Pliki','sequence.fasta',...
-    'sequence2.fasta','sequence3.fasta','sequence4.fasta')
+% MultipleSequencesAlignment(0,2,1,4,'MSAalignment.txt',...
+%     'C:\Nauka\6\Wstêp do bioinformatyki\Lab\Pliki','sequence.fasta',...
+%     'sequence2.fasta','sequence3.fasta','sequence4.fasta')
 
